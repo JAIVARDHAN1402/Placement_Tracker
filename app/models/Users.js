ab663691210bb83761c57e6ca46dcd05cd523216
Jaivardhan Singh <jaivardhan1402@gmail.com>
2026-07-29 21:50:12 +0530

    Update Users.js

--- app/models/Users.js
+++ app/models/Users.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+// ye  toh scheme design hua h
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
@@ -12,15 +13,18 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function () {
+// ye pwd ko hashing krne k liye -- db mei save hone se phle hash ho uske baad store ho
+userSchema.pre("save", async function () { // pre ka wahi mtlb h store hone se phle hash ho jaaye pwd
   if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12); // 12 number of times hash kr rha h pwd ko
 });
 
+// hashed pwd ko compare krre h dekhne k liye ki sahi login credential h n
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
+// ye krre qki server se client ko data jata h usmei pwd hidden rhe iske liye ye krre h
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.password;
